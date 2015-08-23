64dd5a31535fb68e11345fcd6bbe15fd9d81c2d5
Adrian <dqzhu@ideabinder.com>
2015-08-23 10:44:35 +0800

    static table view

---
+++ StaticTableDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StaticTableDemo
+//
+//  Created by Adrian on 15/8/22.
+//  Copyright (c) 2015年 Adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StaticTableDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticTableDemo
+//
+//  Created by Adrian on 15/8/22.
+//  Copyright (c) 2015年 Adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
